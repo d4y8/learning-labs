a82371f9774fcb0ef9d47a3a5c7f296f0183806d
yamahad <yamahashi.d@gmail.com>
2025-01-13 17:26:05 +0900

    ワークフロー検証

--- .github/scripts/blog_post-oauth.py
+++ .github/scripts/blog_post-oauth.py
@@ -1,60 +1,63 @@
-import os
-import glob
-import httplib2
-
+import pickle
+import os.path
 from googleapiclient.discovery import build
-from oauth2client import file, client, tools
-
-blogger_client_id = os.environ['blogger_client_id']
-blogger_client_secret = os.environ['blogger_client_secret']
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
-credential_path = 'credentials.json'  # 認証情報を保存するファイルパス
 
-blog_id = os.environ.get("BLOG_ID")
-blogger_post(blog_id=blog_id, title="test", contest="test")
+API_SERVICE_NAME = "blogger"
+API_VERSION = "v3"
+SCOPES = ["https://www.googleapis.com/auth/blogger"]
+CLIENT_SECRETS_FILE = 'client_secrets.json'
 
-def blogger_post(blog_id, title, content, labels, isDraft=True):
-    if content is None:
-        print('コンテンツがないため作成できません')
-        return None
 
-    # OAuth 2.0の認証フローを設定
-    flow = client.OAuth2WebServerFlow(blogger_client_id, blogger_client_secret, 'https://www.googleapis.com/auth/blogger', redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-    storage = file.Storage(credential_path)
-    credentials = storage.get()
+def get_authenticated_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRETS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-    if credentials is None or credentials.invalid:
-        credentials = tools.run_flow(flow, storage)
+    return build(API_SERVICE_NAME, API_VERSION, credentials=creds)
 
-    # 認証情報を使用してBlogger APIを初期化
-    http = credentials.authorize(httplib2.Http())
-    service = build('blogger', 'v3', http=http)
 
-    # 新しい投稿を作成するためのデータ
-    post_data = {
-        "kind": "blogger#post",
-        "blog": {
-            "id": blog_id
-        },
-        "title": title,
-        "content": content,
-        "labels": labels,
+if __name__ == "__main__":
+    service = get_authenticated_service()
+
+    body = {
+        "kind": "blogger#page",
+        "content": "test",
+        "title": "Title"
     }
 
-    # 投稿を作成
-    try:
-        if isDraft:
-            new_post = service.posts().insert(blogId=blog_id, body=post_data, isDraft=True).execute()
-            print("新しい下書きが作成されました。投稿のID:", new_post['url'])
-        else:
-            new_post = service.posts().insert(blogId=blog_id, body=post_data).execute()
-            print("新しい投稿が作成されました。投稿のID:", new_post['url'])
-        return True
-    except Exception as e:
-        print("投稿の作成中にエラーが発生しました:", str(e))
-        return False
-    
+    blog_id = os.environ.get("BLOG_ID")
+    # listは権限あり
+    results = service.posts().list(blogId=blog_id).execute()
+    print(results)
+
+    # insertは403エラー
+    result = service.posts().insert(blogId=blog_id).execute()
+    print(result)
+
+
+
+
+    # youtube.hoge() ... 動画を検索したり、アップロードしたり、再生リスト作ったり...
+    # サンプルコード: https://developers.google.com/youtube/v3/code_samples?hl=ja#python
 
 # #必要なスコープを指定
 # # SCOPES = ["https://www.googleapis.com/auth/blogger"] 
